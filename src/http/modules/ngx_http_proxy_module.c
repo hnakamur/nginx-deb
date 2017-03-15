2f55487f82ee7acdb5c00cfe5e0458c85d56b514
Piotr Sikora <piotrsikora@google.com>
2017-03-15 15:54:49 -0700

    Proxy: always emit "Host" header first.
    Origin: http://mailman.nginx.org/pipermail/nginx-devel/2017-June/010218.html
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>
    
    Gbp-Pq: Name 0010-Proxy-always-emit-Host-header-first.patch

--- src/http/modules/ngx_http_proxy_module.c
+++ src/http/modules/ngx_http_proxy_module.c
@@ -3443,7 +3443,7 @@ ngx_http_proxy_init_headers(ngx_conf_t *cf, ngx_http_proxy_loc_conf_t *conf,
     uintptr_t                    *code;
     ngx_uint_t                    i;
     ngx_array_t                   headers_names, headers_merged;
-    ngx_keyval_t                 *src, *s, *h;
+    ngx_keyval_t                 *host, *src, *s, *h;
     ngx_hash_key_t               *hk;
     ngx_hash_init_t               hash;
     ngx_http_script_compile_t     sc;
@@ -3475,10 +3475,32 @@ ngx_http_proxy_init_headers(ngx_conf_t *cf, ngx_http_proxy_loc_conf_t *conf,
         return NGX_ERROR;
     }
 
+    h = default_headers;
+
+    if (h->key.len != sizeof("Host") - 1
+        || ngx_strcasecmp(h->key.data, (u_char *) "Host") != 0)
+    {
+        return NGX_ERROR;
+    }
+
+    host = ngx_array_push(&headers_merged);
+    if (host == NULL) {
+        return NGX_ERROR;
+    }
+
+    *host = *h++;
+
     if (conf->headers_source) {
 
         src = conf->headers_source->elts;
         for (i = 0; i < conf->headers_source->nelts; i++) {
+
+            if (src[i].key.len == sizeof("Host") - 1
+                && ngx_strcasecmp(src[i].key.data, (u_char *) "Host") == 0)
+            {
+                *host = src[i];
+                continue;
+            }
 
             s = ngx_array_push(&headers_merged);
             if (s == NULL) {
@@ -3488,8 +3510,6 @@ ngx_http_proxy_init_headers(ngx_conf_t *cf, ngx_http_proxy_loc_conf_t *conf,
             *s = src[i];
         }
     }
-
-    h = default_headers;
 
     while (h->key.len) {
 
