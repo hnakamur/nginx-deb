3589fb56fcb0cec212e7caadfa39268549d18977
Hiroaki Nakamura <hnakamur@gmail.com>
2017-07-15 23:06:54 +0900

    Modify github repo download script

--- debian/download-module-sources.sh
+++ debian/download-module-sources.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 ngx_lua_commit=a5094766702d97ab43c742f26d6d55098aa1b1f0
 
-download_github_repo_master() {
+download_github_repo() {
   local user=$1
   local repo=$2
+  local commit=$3
 
   if [ ! -d $repo ]; then
-    local commit=$(curl -sS https://api.github.com/repos/$user/$repo/commits/master | jq -r .sha)
-    curl -sSL https://github.com/$user/$repo/archive/master.tar.gz | tar zxf -
-    mv ${repo}-master $repo
-    echo $user $repo $commit
+    if [ $commit = master ]; then
+      commit=$(curl -sS https://api.github.com/repos/$user/$repo/commits/master | jq -r .sha)
+    fi
+    curl -sSL https://github.com/$user/$repo/archive/${commit}.tar.gz | tar zxf -
+    mv ${repo}-${commit} $repo
+    echo $user/$repo $commit
   fi
 }
 
-if [ ! -d lua-nginx-module ]; then
-  curl -sSL https://github.com/openresty/lua-nginx-module/archive/${ngx_lua_commit}.tar.gz | tar zxf -
-  mv lua-nginx-module-${ngx_lua_commit} lua-nginx-module
-fi
-
-download_github_repo_master openresty headers-more-nginx-module
+download_github_repo openresty lua-nginx-module a5094766702d97ab43c742f26d6d55098aa1b1f0
+download_github_repo openresty headers-more-nginx-module master
+download_github_repo openresty replay ngx_http_secure_download master
+download_github_repo openresty replay ngx_http_consistent_hash master
