6e633b208b0718192f0daed6a84450810c26748f
Hiroaki Nakamura <hnakamur@gmail.com>
2020-04-15 09:06:12 +0900

    Add module sources
    openresty/echo-nginx-module a47a9817660db383a996f4683f2375a8735140a5
    openresty/headers-more-nginx-module 743a4bb1a253325d17a4f4ce8ee61ea0d8e0cc19
    openresty/lua-nginx-module 896638287afd83f060d3f8e9a51b53dbd26d0ae7
    openresty/lua-upstream-nginx-module 2f012de74710da7750ed56ed3cb53ded2d62fc12
    openresty/memc-nginx-module dfbed06016b73e3e84cd961735a352e4f5477865
    openresty/redis2-nginx-module cc146f37440f798ffc41c3cb792eca3c60bc62d3
    openresty/set-misc-nginx-module 5cc144d7b3387f4bf90ad92525985c835255debd
    openresty/srcache-nginx-module 824f691819a587910b72c19c6374c79d61630594
    openresty/stream-lua-nginx-module 61cfab814418df78638c6d3c68dfd326e37bdf8c
    openresty/lua-resty-balancer 3807cd5501f3702f0d24bdba465d98e32d29c219
    openresty/lua-resty-core 1bd74f8f502a9dc0a7099110d0ee184187698d01
    openresty/lua-resty-dns cddf8cd81cda5ff94e5aab26bb622f0384d0324a
    openresty/lua-resty-lock 987b0e9c6748e2af0f4300309ab8c94df5be9692
    openresty/lua-resty-limit-traffic 2c5a23196360d533883cbfa03b3543fc7dfba4d5
    openresty/lua-resty-lrucache b2035269ac353444ac65af3969692bcae4fc1605
    openresty/lua-resty-memcached fe57a4ad265deeaa3a7ce57cc77c2c074f2bd48c
    openresty/lua-resty-memcached-shdict 32374a1a286506cf4cf9c18a8c1bf01bd1554c21
    openresty/lua-resty-mysql 180db30817020356acfe810ea666d1db567cf65d
    openresty/lua-resty-redis 057093e8f8c33974c3acdc941404158cb6a2c2a2
    openresty/lua-resty-shdict-simple 4d27246b16f86de49e6da8b8a6136cddfe7550b4
    openresty/lua-resty-shell ea23b3d6d4a4b3c475a3e0b530469343694e775c
    openresty/lua-resty-signal 6b1bf606563c18eb4a25a7f36e9e2337af9217c0
    openresty/lua-resty-string 9a543f8531241745f8814e8e02475351042774ec
    openresty/lua-resty-upload 8f4f0f8cb1edc97fb39c16d256968e5b1a18f9ea
    openresty/lua-resty-upstream-healthcheck 26bad5df8131d93c6f06011cce82b80929ad8340
    openresty/lua-resty-websocket 739183cd638eb069af2686682a3810c28d822e82
    cloudflare/lua-resty-cookie c54865bdcfc3c42cbd6dbbceb654ba73871d07f6
    zmartzone/lua-resty-openidc b9fd68e20da0429a9c5c3061a4d1fdce6c799423
    bungle/lua-resty-session c6e246d51def4926a803ee0ff35e46890f41b497
    cdbattags/lua-resty-jwt 381ccbaa6649f35ae57d6ffaa2d5120a1bafd8dd
    jkeys089/lua-resty-hmac 5f60e1a28b9fdd7939f0e1738171f0ca5fe22978
    pintsized/lua-resty-http a6bd2e0eb1390e330e4fb10a48cced5a1f21fb66
    FRiCKLE/ngx_cache_purge 331fe43e8d9a3d1fa5e0c9fec7d3201d431a9177
    arut/nginx-rtmp-module 791b6136f02bc9613daf178723ac09f4df5a3bbf
    arut/nginx-dav-ext-module f5e30888a256136d9c550bf1ada77d6ea78a48af
    bpaquet/ngx_http_enhanced_memcached_module b58a4500db3c4ee274be54a18abc767219dcfd36
    replay/ngx_http_secure_download f379a1acf2a76f63431a12fa483d9e22e718400b
    simplresty/ngx_devel_kit a22dade76c838e5f377d58d007f65d35b5ce1df3
    wandenberg/nginx-sorted-querystring-module e5bbded07fd67e2977edc2bc145c45a7b3fc4d26
    pandax381/ngx_http_pipelog_module 68b5f4b6553e1588c42d2cf021aa8b5f7bf268fb
    nginx-shib/nginx-http-shibboleth 473270289dcecced713025d5d77bae901389506b
    hnakamur/nginx-lua-saml-service-provider e5256d0008210318cacf6bbd9b29bfa693b78811
    hnakamur/nginx-lua-session db89c135fd920410cac31be60afb17d511851bfd
    hamishforbes/lua-ffi-zlib 1fb69ca505444097c82d2b72e87904f3ed923ae9
    Phrogz/SLAXML 108970cbcb4370365e79c619b0f06099c9ec9754
    leev/ngx_http_geoip2_module 1cabd8a1f68ea3998f94e9f3504431970f848fbf
    e98cuenc/ngx_upstream_jdomain a5a8e07f76cbe82b0792f8e3091e011c0c12f230
    google/nginx-sxg-module 3f57fc0f5bf5dc538fa76cbf27d058f4e00e45c5
    woothee/lua-resty-woothee 0e594c4582f5e47a3eef1c755c59e5c8ae86d268
    ruoshan/lua-resty-jump-consistent-hash a01d2683bfe34cc4edaab7ecac7906d51dfbd125

---
+++ lua-resty-session/lib/resty/session/storage/redis.lua
@@ -0,0 +1,302 @@
+local redis        = require "resty.redis"
+
+local setmetatable = setmetatable
+local tonumber     = tonumber
+local concat       = table.concat
+local sleep        = ngx.sleep
+local null         = ngx.null
+local var          = ngx.var
+
+local UNLOCK = [[
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+    return redis.call("DEL", KEYS[1])
+else
+    return 0
+end
+]]
+
+local function enabled(value)
+    if value == nil then return nil end
+    return value == true or (value == "1" or value == "true" or value == "on")
+end
+
+local defaults = {
+    prefix       = var.session_redis_prefix                      or "sessions",
+    database     = tonumber(var.session_redis_database,     10)  or 0,
+    socket       = var.session_redis_socket,
+    host         = var.session_redis_host                        or "127.0.0.1",
+    port         = tonumber(var.session_redis_port,         10)  or 6379,
+    auth         = var.session_redis_auth,
+    uselocking   = enabled(var.session_redis_uselocking          or true),
+    spinlockwait = tonumber(var.session_redis_spinlockwait, 10)  or 150,
+    maxlockwait  = tonumber(var.session_redis_maxlockwait,  10)  or 30,
+    pool = {
+        timeout  = tonumber(var.session_redis_pool_timeout, 10),
+        size     = tonumber(var.session_redis_pool_size,    10)
+    },
+    ssl          = enabled(var.session_redis_ssl)                or false,
+    ssl_verify   = enabled(var.session_redis_ssl_verify)         or false,
+    server_name  = var.session_redis_server_name,
+}
+
+local storage = {}
+
+storage.__index = storage
+
+function storage.new(session)
+    local config = session.redis or defaults
+    local pool   = config.pool   or defaults.pool
+
+    local locking = enabled(config.uselocking)
+    if locking == nil then
+        locking = defaults.uselocking
+    end
+
+    local self = {
+        redis         = redis:new(),
+        auth          = config.auth                       or defaults.auth,
+        prefix        = config.prefix                     or defaults.prefix,
+        database      = tonumber(config.database,     10) or defaults.database,
+        uselocking    = locking,
+        spinlockwait  = tonumber(config.spinlockwait, 10) or defaults.spinlockwait,
+        maxlockwait   = tonumber(config.maxlockwait,  10) or defaults.maxlockwait,
+        pool = {
+            timeout   = tonumber(pool.timeout,        10) or defaults.pool.timeout,
+            size      = tonumber(pool.size,           10) or defaults.pool.size,
+        },
+        connect_opts = {
+          ssl         = config.ssl                        or defaults.ssl,
+          ssl_verify  = config.ssl_verify                 or defaults.ssl_verify,
+          server_name = config.server_name                or defaults.server_name,
+        },
+    }
+
+    local socket = config.socket or defaults.socket
+    if socket and socket ~= "" then
+        self.socket = socket
+    else
+        self.host = config.host or defaults.host
+        self.port = config.port or defaults.port
+    end
+
+    return setmetatable(self, storage)
+end
+
+function storage:connect()
+    local ok, err
+    if self.socket then
+        ok, err = self.redis:connect(self.socket)
+    else
+        ok, err = self.redis:connect(self.host, self.port, self.connect_opts)
+    end
+
+    if not ok then
+        return nil, err
+    end
+
+    if self.auth and self.auth ~= "" and self.redis:get_reused_times() == 0 then
+        ok, err = self.redis:auth(self.auth)
+        if not ok then
+            return nil, err
+        end
+    end
+
+    if self.database ~= 0 then
+        ok, err = self.redis:select(self.database)
+    end
+
+    return ok, err
+end
+
+function storage:set_keepalive()
+    local pool    = self.pool
+    local timeout = pool.timeout
+    local size    = pool.size
+
+    if timeout and size then
+        return self.redis:set_keepalive(timeout, size)
+    end
+
+    if timeout then
+        return self.redis:set_keepalive(timeout)
+    end
+
+    return self.redis:set_keepalive()
+end
+
+function storage:key(id)
+    return concat({ self.prefix, id }, ":" )
+end
+
+function storage:lock(key)
+    if not self.uselocking or self.locked then
+        return true
+    end
+
+    if not self.token then
+        self.token = var.request_id
+    end
+
+    local lock_key = concat({ key, "lock" }, "." )
+    local lock_ttl = self.maxlockwait + 1
+    local attempts = (1000 / self.spinlockwait) * self.maxlockwait
+    local waittime = self.spinlockwait / 1000
+
+    for _ = 1, attempts do
+        local ok = self.redis:set(lock_key, self.token, "EX", lock_ttl, "NX")
+        if ok ~= null then
+            self.locked = true
+            return true
+        end
+
+        sleep(waittime)
+    end
+
+    return false, "unable to acquire a session lock"
+end
+
+function storage:unlock(key)
+    if not self.uselocking or not self.locked then
+        return
+    end
+
+    local lock_key = concat({ key, "lock" }, "." )
+
+    self.redis:eval(UNLOCK, 1, lock_key, self.token)
+    self.locked = nil
+end
+
+function storage:get(key)
+    local data, err = self.redis:get(key)
+    if not data then
+        return nil, err
+    end
+
+    if data == null then
+        return nil
+    end
+
+    return data
+end
+
+function storage:set(key, data, lifetime)
+    return self.redis:setex(key, lifetime, data)
+end
+
+function storage:expire(key, lifetime)
+    return self.redis:expire(key, lifetime)
+end
+
+function storage:delete(key)
+    return self.redis:del(key)
+end
+
+function storage:open(id)
+    local ok, err = self:connect()
+    if not ok then
+        return nil, err
+    end
+
+    local key = self:key(id)
+
+    ok, err = self:lock(key)
+    if not ok then
+        self:set_keepalive()
+        return nil, err
+    end
+
+    local data
+    data, err = self:get(key)
+
+    self:unlock(key)
+    self:set_keepalive()
+
+    return data, err
+end
+
+function storage:start(id)
+    local ok, err = self:connect()
+    if not ok then
+        return nil, err
+    end
+
+    ok, err = self:lock(self:key(id))
+
+    self:set_keepalive()
+
+    return ok, err
+end
+
+function storage:save(id, ttl, data, close)
+    local ok, err = self:connect()
+    if not ok then
+        return nil, err
+    end
+
+    local key = self:key(id)
+
+    ok, err = self:set(key, data, ttl)
+
+    if close then
+        self:unlock(key)
+    end
+
+    self:set_keepalive()
+
+    if not ok then
+        return nil, err
+    end
+
+    return true
+end
+
+function storage:close(id)
+    local ok, err = self:connect()
+    if not ok then
+        return nil, err
+    end
+
+    local key = self:key(id)
+
+    self:unlock(key)
+    self:set_keepalive()
+
+    return true
+end
+
+function storage:destroy(id)
+    local ok, err = self:connect()
+    if not ok then
+        return nil, err
+    end
+
+    local key = self:key(id)
+
+    ok, err = self:delete(key)
+
+    self:unlock(key)
+    self:set_keepalive()
+
+    return ok, err
+end
+
+function storage:ttl(id, ttl, close)
+    local ok, err = self:connect()
+    if not ok then
+        return nil, err
+    end
+
+    local key = self:key(id)
+
+    ok, err = self:expire(key, ttl)
+
+    if close then
+        self:unlock(key)
+    end
+
+    self:set_keepalive()
+
+    return ok, err
+end
+
+return storage
---
+++ nginx-sxg-module/ngx_sxg_utils.c
@@ -0,0 +1,355 @@
+// Copyright 2019 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include "ngx_sxg_utils.h"
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <strings.h>
+
+#ifdef __USE_THIRD_PARTY__
+#include "third_party/openssl/ocsp.h"
+#include "third_party/openssl/pem.h"
+#else
+#include "openssl/ocsp.h"
+#include "openssl/pem.h"
+#endif
+
+#include "libsxg.h"
+
+size_t get_term_length(const char* str, size_t len, char delimiter,
+                       const char quotes[2]) {
+  bool inside_quote = false;
+  for (size_t i = 0; i < len; ++i) {
+    if (!inside_quote) {
+      if (str[i] == quotes[0]) {
+        inside_quote = true;
+      } else if (str[i] == delimiter) {
+        return i;
+      }
+    } else if (inside_quote && str[i] == quotes[1]) {
+      inside_quote = false;
+    }
+  }
+  return len;
+}
+
+// Returns value pointer for spefified |key|. Skips prefix whitespace or tab.
+// |key| is case insensitive.
+static const char* extract_prefix_with_whitespace(const char* key,
+                                                  const char* str,
+                                                  size_t strlength) {
+  if (strlength == 0) {
+    return NULL;
+  }
+  const size_t keylength = strlen(key);
+  // Omit optional white space prefix.
+  while (strlength > 0 && (*str == ' ' || *str == '\t')) {
+    ++str;
+    --strlength;
+  }
+  if (keylength <= strlength && strncasecmp(key, str, keylength) == 0) {
+    return str + keylength;
+  }
+  return NULL;
+}
+
+// Returns true if the first word of |str| equals |expected| after optional
+// double-quotation is removed. Whitespace means end of value.
+static bool first_word_match(const char* expected, const char* str,
+                             size_t len) {
+  const size_t expected_length = strlen(expected);
+  if (len == 0 || (str[0] == '"' && len == 1)) {
+    return false;
+  }
+  if (str[0] == '"' && str[len - 1] == '"') {
+    str += 1;
+    len -= 2;
+  }
+  if (len < expected_length) {
+    return false;
+  }
+  return strncmp(str, expected, expected_length) == 0 &&
+         (len == expected_length || str[expected_length] == ' ');
+}
+
+static bool term_is_sxg(const char* str, size_t len) {
+  static const char kSxg[] = "application/signed-exchange";
+  static const char kValueKey[] = "v=";
+  static const char kValidVersion[] = "b3";
+  const char* const end = str + len;
+  const size_t media_range_tail = get_term_length(str, len, ';', "<>");
+  if (len < strlen(kSxg) || strncmp(kSxg, str, strlen(kSxg)) != 0 ||
+      len == media_range_tail) {  // The version information is missing.
+    return false;
+  }
+  str += media_range_tail + 1;
+  while (str < end) {
+    const size_t tail = get_term_length(str, end - str, ';', "\"\"");
+    const char* const value_ptr =
+        extract_prefix_with_whitespace(kValueKey, str, tail);
+    if (value_ptr != NULL) {
+      return first_word_match(kValidVersion, value_ptr,
+                              tail - (value_ptr - str));
+    }
+    str += tail + 1;
+  }
+  return false;
+}
+
+// Returns qvalue * 1000.
+// Returns 0 if qvalue does not match the RFC7231 spec.
+static int parse_qvalue(const char* str, size_t len) {
+  // https://tools.ietf.org/html/rfc7231#section-5.3.1 says that
+  // qvalue MUST be between 0.000 and 1.000.
+  while (len > 0 && (str[len - 1] == ' ' || str[len - 1] == '\t')) {
+    --len;
+  }
+  if (len == 1 && *str == '1') {
+    return 1000;
+  }
+  if (len == 0 || len > 5) {  // Contains no digits or extra digits.
+    return 0;
+  }
+  char data[6] = {};  // 1 digit + '.' + 3 digits + null
+  memcpy(data, str, len);
+  if (strspn(data, "1234567890.") != len) {  // Contains non-digit word.
+    return 0;
+  }
+  memset(data + len, '0', sizeof(data) - len - 1);
+  int whole = 0, part = 0;
+  if (sscanf(data, "%d.%d", &whole, &part) != 2) {
+    return 0;
+  }
+  int qvalue = whole * 1000 + part;
+  return qvalue > 0 && qvalue <= 1000 ? qvalue : 0;
+}
+
+// Find and return qvalue.
+// e.g. text/html;q=0.8 -> 800
+//      image/png; -> 1000 (Omittion means MAX)
+static int get_priority(const char* str, size_t len) {
+  for (;;) {
+    const size_t tail = get_term_length(str, len, ';', "\"\"");
+    const char* value_ptr = extract_prefix_with_whitespace("q=", str, tail);
+    if (value_ptr != NULL) {
+      return parse_qvalue(value_ptr, tail - (value_ptr - str));
+    }
+    str += tail;
+    len -= tail;
+    if (len == 0) {
+      break;
+    }
+    ++str;
+    --len;
+  }
+  return 1000;
+}
+
+bool highest_qvalue_is_sxg(const char* str, size_t len) {
+  int max_priority = 0;
+  bool should_be_sxg = false;
+  for (const char* const end = str + len; str < end;) {
+    const size_t tail = get_term_length(str, len, ',', "<>");
+    const bool is_sxg = term_is_sxg(str, tail);
+    const int p = get_priority(str, tail);
+    if (max_priority < p || (max_priority == p && is_sxg)) {
+      should_be_sxg = is_sxg;
+      max_priority = p;
+    }
+    str += tail + 1;
+    len -= tail + 1;
+  }
+  return should_be_sxg;
+}
+
+// Finds |desired| word exists in quoted and space-separated string.
+// The |desired| word must not contain space.
+// |desired| must be null terminated.
+static bool quoted_string_match(const char* str, size_t len,
+                                const char* desired) {
+  if (str[0] != '"' || str[len - 1] != '"') {
+    return false;
+  }
+  ++str;     // Skip '"'.
+  len -= 2;  // Omit first and last '"'.
+  const size_t desired_length = strlen(desired);
+  const char* const end = str + len;
+  while (str < end) {
+    size_t tail = get_term_length(str, len, ' ', "<>");
+    if (tail == desired_length && memcmp(str, desired, desired_length) == 0) {
+      return true;
+    }
+    str += tail + 1;
+    len -= tail + 1;
+  }
+  return false;
+}
+
+bool param_is_preload(const char* param, size_t len) {
+  static const char kRel[] = "rel=";
+  static const char kPreload[] = "preload";
+  if (len < sizeof(kRel) - 1 ||
+      strncmp((char*)param, kRel, sizeof(kRel) - 1) != 0) {
+    return false;
+  }
+  param += sizeof(kRel) - 1;
+  len -= sizeof(kRel) - 1;
+  if (len > 0 && *param == '"') {
+    return quoted_string_match(param, len, kPreload);
+  } else {
+    return len == sizeof(kPreload) - 1 && memcmp(param, kPreload, len) == 0;
+  }
+}
+
+EVP_PKEY* load_private_key(const char* filepath) {
+  FILE* const fp = fopen(filepath, "r");
+  if (!fp) {
+    return NULL;
+  }
+  EVP_PKEY* private_key = PEM_read_PrivateKey(fp, NULL, NULL, NULL);
+  fclose(fp);
+  return private_key;
+}
+
+X509* load_x509_cert(const char* filepath) {
+  FILE* certfile = fopen(filepath, "r");
+  if (!certfile) {
+    return NULL;
+  }
+  char passwd = 0;
+  X509* cert = PEM_read_X509(certfile, 0, 0, &passwd);
+  fclose(certfile);
+  return cert;
+}
+
+ngx_sxg_cert_chain_t ngx_sxg_empty_cert_chain() {
+  static ngx_sxg_cert_chain_t empty;
+  empty.serialized_cert_chain = sxg_empty_buffer();
+  empty.certificate = NULL;
+  empty.issuer = NULL;
+  empty.ocsp = NULL;
+  return empty;
+}
+
+void ngx_sxg_cert_chain_release(ngx_sxg_cert_chain_t* target) {
+  sxg_buffer_release(&target->serialized_cert_chain);
+  if (target->certificate != NULL) {
+    X509_free(target->certificate);
+    target->certificate = NULL;
+  }
+  if (target->issuer != NULL) {
+    X509_free(target->issuer);
+    target->issuer = NULL;
+  }
+}
+
+bool load_cert_chain(const char* cert_path, ngx_sxg_cert_chain_t* target) {
+  FILE* certfile = fopen(cert_path, "r");
+  if (!certfile) {
+    return false;
+  }
+  char passwd = 0;
+  X509* cert = PEM_read_X509(certfile, 0, 0, &passwd);
+  X509* issuer = PEM_read_X509(certfile, 0, 0, &passwd);
+  fclose(certfile);
+
+  target->certificate = cert;
+  target->issuer = issuer;
+  return cert != NULL && issuer != NULL;
+}
+
+bool write_cert_chain(ngx_sxg_cert_chain_t* cert, sxg_buffer_t* dst) {
+  sxg_buffer_t empty_sct_list = sxg_empty_buffer();
+  sxg_cert_chain_t chain = sxg_empty_cert_chain();
+  bool success =
+      sxg_cert_chain_append_cert(cert->certificate, cert->ocsp, &empty_sct_list,
+                                 &chain) &&
+      sxg_cert_chain_append_cert(cert->issuer, NULL, &empty_sct_list, &chain) &&
+      sxg_write_cert_chain_cbor(&chain, dst);
+  sxg_cert_chain_release(&chain);
+  return success;
+}
+
+static bool asn1_generalizedtime_to_unixtime(const ASN1_GENERALIZEDTIME* ag,
+                                             time_t* out) {
+  if (ag->type != V_ASN1_GENERALIZEDTIME) {
+    return false;
+  }
+  struct tm t;
+  if (ASN1_TIME_to_tm(ag, &t) != 1) {
+    return false;
+  }
+  *out = mktime(&t);
+  return true;
+}
+
+static bool check_refresh_needed(ngx_sxg_cert_chain_t* target) {
+  if (target->ocsp == NULL) {
+    return true;
+  }
+  OCSP_BASICRESP* br = OCSP_response_get1_basic(target->ocsp);
+  const int resps = OCSP_resp_count(br);
+  for (int i = 0; i < resps; ++i) {
+    OCSP_SINGLERESP* leaf = OCSP_resp_get0(br, i);
+    int revocation_reason;
+    ASN1_GENERALIZEDTIME* revocation_time;
+    ASN1_GENERALIZEDTIME* this_update;
+    ASN1_GENERALIZEDTIME* next_update;
+
+    int status = OCSP_single_get0_status(
+        leaf, &revocation_reason, &revocation_time, &this_update, &next_update);
+    if (status == -1) {
+      return true;
+    }
+
+    time_t since;
+    time_t until;
+    time_t update;
+    if (!asn1_generalizedtime_to_unixtime(this_update, &since) ||
+        !asn1_generalizedtime_to_unixtime(next_update, &until)) {
+      return true;
+    }
+
+    // NextUpdate field is optional.
+    bool next_update_exists =
+        asn1_generalizedtime_to_unixtime(revocation_time, &update);
+
+    if (since < until) {
+      return false;
+    }
+    time_t middle_lifespan = since + ((until - since) / 2);
+    const time_t now = time(NULL);
+
+    if (middle_lifespan < now || (next_update_exists && update < now)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool refresh_if_needed(ngx_sxg_cert_chain_t* target) {
+  if (!check_refresh_needed(target)) {
+    return false;
+  }
+  if (target->ocsp != NULL) {
+    OCSP_RESPONSE_free(target->ocsp);
+  }
+  return sxg_fetch_ocsp_response(target->certificate, target->issuer,
+                                 &target->ocsp) &&
+         write_cert_chain(target, &target->serialized_cert_chain);
+}
