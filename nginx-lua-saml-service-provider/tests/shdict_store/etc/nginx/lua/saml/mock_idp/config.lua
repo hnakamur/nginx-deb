ba2e5ea09f4b3c8f418f3e4a61da10e23d163514
Hiroaki Nakamura <hnakamur@gmail.com>
2020-04-22 08:31:36 +0900

    delete removed module files

--- nginx-lua-saml-service-provider/tests/shdict_store/etc/nginx/lua/saml/mock_idp/config.lua
+++
@@ -1,9 +0,0 @@
-return {
-    key_attribute_name = "mail",
-    response = {
-        id_attr = {
-            attrName = "ID", nodeName = "Response",
-            nsHref = "urn:oasis:names:tc:SAML:2.0:protocol"
-        }
-    }
-}
