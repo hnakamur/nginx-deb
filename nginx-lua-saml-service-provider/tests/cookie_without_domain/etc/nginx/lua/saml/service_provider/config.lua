1c8950e967827a3e3bdc81d3b9ae11f14921aa81
Hiroaki Nakamura <hnakamur@gmail.com>
2020-07-08 03:04:00 +0900

    Add module sources
    openresty/echo-nginx-module 08ad67787d19235cb298f061e8950afbb6fd48b9
    openresty/headers-more-nginx-module 743a4bb1a253325d17a4f4ce8ee61ea0d8e0cc19
    openresty/lua-nginx-module 635b6e864c385bc16125bd43a2ffe8b4560ca9fa
    openresty/lua-upstream-nginx-module 2f012de74710da7750ed56ed3cb53ded2d62fc12
    openresty/memc-nginx-module dfbed06016b73e3e84cd961735a352e4f5477865
    openresty/redis2-nginx-module cc146f37440f798ffc41c3cb792eca3c60bc62d3
    openresty/set-misc-nginx-module 4667684cfed0f523596895d338f376ffd5f72879
    openresty/srcache-nginx-module ab46d947e2fda591de06dac3ba6c9c6f0e8f6b61
    openresty/stream-lua-nginx-module 5a8bc299acf365f9511537895482cc29c161482a
    openresty/lua-resty-balancer 3807cd5501f3702f0d24bdba465d98e32d29c219
    openresty/lua-resty-core b7d0a681bb41e6e3f29e8ddc438ef26fd819bb19
    openresty/lua-resty-dns cddf8cd81cda5ff94e5aab26bb622f0384d0324a
    openresty/lua-resty-lock 987b0e9c6748e2af0f4300309ab8c94df5be9692
    openresty/lua-resty-limit-traffic bd1cc13b0a635fd057bc639dc56d26bd0cd3ee74
    openresty/lua-resty-lrucache b2035269ac353444ac65af3969692bcae4fc1605
    openresty/lua-resty-memcached d4b0186d5e2f65e38c3c63408d462d98c8dc5977
    openresty/lua-resty-memcached-shdict 32374a1a286506cf4cf9c18a8c1bf01bd1554c21
    openresty/lua-resty-mysql a46c69b40fc49ee16ad99935ff3858fee05798f7
    openresty/lua-resty-redis e2db038fccc48674a4b3956f3a9de5197b235505
    openresty/lua-resty-shdict-simple 4d27246b16f86de49e6da8b8a6136cddfe7550b4
    openresty/lua-resty-shell ea23b3d6d4a4b3c475a3e0b530469343694e775c
    openresty/lua-resty-signal 6b1bf606563c18eb4a25a7f36e9e2337af9217c0
    openresty/lua-resty-string 9a543f8531241745f8814e8e02475351042774ec
    openresty/lua-resty-upload 8f4f0f8cb1edc97fb39c16d256968e5b1a18f9ea
    openresty/lua-resty-upstream-healthcheck 26bad5df8131d93c6f06011cce82b80929ad8340
    openresty/lua-resty-websocket cdb16f2434819c61ca0db6792db1c7069bcf5e06
    cloudflare/lua-resty-cookie c54865bdcfc3c42cbd6dbbceb654ba73871d07f6
    zmartzone/lua-resty-openidc 9155766ea354cb74c6fe496eaa7545e822f60297
    bungle/lua-resty-session 3e4a3b9fbae76d9621b41db760989345f623f8f2
    cdbattags/lua-resty-jwt d1798eb236cac5cce7e273ee8d48747739a03754
    jkeys089/lua-resty-hmac 696db6e747acf3dde995bed098837c9f608cc748
    pintsized/lua-resty-http a6bd2e0eb1390e330e4fb10a48cced5a1f21fb66
    FRiCKLE/ngx_cache_purge 331fe43e8d9a3d1fa5e0c9fec7d3201d431a9177
    arut/nginx-rtmp-module 791b6136f02bc9613daf178723ac09f4df5a3bbf
    arut/nginx-dav-ext-module f5e30888a256136d9c550bf1ada77d6ea78a48af
    bpaquet/ngx_http_enhanced_memcached_module b58a4500db3c4ee274be54a18abc767219dcfd36
    replay/ngx_http_secure_download f379a1acf2a76f63431a12fa483d9e22e718400b
    simplresty/ngx_devel_kit a22dade76c838e5f377d58d007f65d35b5ce1df3
    wandenberg/nginx-sorted-querystring-module e5bbded07fd67e2977edc2bc145c45a7b3fc4d26
    pandax381/ngx_http_pipelog_module 68b5f4b6553e1588c42d2cf021aa8b5f7bf268fb
    nginx-shib/nginx-http-shibboleth 0c7987f54931b71ec9a50b1e541bf24f256520c7
    hnakamur/nginx-lua-saml-service-provider ae2c02dc4c56b11d7536d8f7b7d25ef152edf7c7
    hnakamur/nginx-lua-session 9c5053a7c9d2e8748f1153193d598dcea16417f4
    hamishforbes/lua-ffi-zlib 1fb69ca505444097c82d2b72e87904f3ed923ae9
    Phrogz/SLAXML 108970cbcb4370365e79c619b0f06099c9ec9754
    leev/ngx_http_geoip2_module 1cabd8a1f68ea3998f94e9f3504431970f848fbf
    e98cuenc/ngx_upstream_jdomain a5a8e07f76cbe82b0792f8e3091e011c0c12f230
    google/nginx-sxg-module 93b5a903cb6bbdf2d22696a74ada5ff9f53dca03
    woothee/lua-resty-woothee 0e594c4582f5e47a3eef1c755c59e5c8ae86d268
    ruoshan/lua-resty-jump-consistent-hash a01d2683bfe34cc4edaab7ecac7906d51dfbd125

---
+++ nginx-lua-saml-service-provider/tests/cookie_without_domain/etc/nginx/lua/saml/service_provider/config.lua
@@ -0,0 +1,46 @@
+return {
+    request = {
+        idp_dest_url = "https://idp.example.com/mock-idp",
+        sp_entity_id = "https://sp.example.com/sso",
+        sp_saml_finish_url = "https://sp.example.com/sso/finish-login",
+        id = {
+            prefix = "_",
+            random_byte_len = 16,
+            expire_seconds = 5 * 60, -- 5 minutes
+        },
+        cookie = {
+            name = "sso_request",
+            path = "/",
+        },
+    },
+    response = {
+        id_attr = {
+            attrName = "ID",
+            nsHref = "urn:oasis:names:tc:SAML:2.0:protocol",
+            nodeName = "Response"
+        },
+        attribute_names = {"mail"},
+    },
+    access_token = {
+        cookie = {
+            name = "sso_access_token",
+            path = "/",
+        },
+    },
+    jwt = {
+        jti = {
+            prefix = "t",
+            random_byte_len = 16,
+        },
+        sign = {
+            algorithm = 'HS256',
+            current_key_id = 'key_2020_001_cea3cd1220254c3914b3012db9707894',
+            keys = {
+                ['key_2020_001_cea3cd1220254c3914b3012db9707894'] = 'Ny5qaJJDXNMjOr+MFFnJoM1LSKr+5F5T',
+            },
+        },
+    },
+    logout = {
+        redirect_url = "/sso/logout-finished"
+    },
+}
