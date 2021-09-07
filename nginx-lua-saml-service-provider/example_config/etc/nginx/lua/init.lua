b00910456a45ca515081d99d7dd11f1eb456d439
Hiroaki Nakamura <hnakamur@gmail.com>
2021-09-08 08:44:47 +0900

    New upstream version 1.21.3

--- nginx-lua-saml-service-provider/example_config/etc/nginx/lua/init.lua
+++
@@ -1,8 +0,0 @@
-(function()
-
-local xmlsec = require "saml.service_provider.xmlsec"
-local config = require "saml.service_provider.config"
-config.response.idp_certificate = xmlsec.readfile('/etc/nginx/saml/idp.example.com.crt')
-xmlsec.load_xsd_files('/etc/nginx/saml')
-
-end)()
