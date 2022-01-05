40768ec5f6bf4988b2388b17fc0bfa4f6c4b7b02
Hiroaki Nakamura <hnakamur@gmail.com>
2022-01-05 11:52:34 +0900

    New upstream version 1.21.5

--- nginx-lua-saml-service-provider/tests/cookie_without_domain/etc/nginx/lua/init.lua
+++
@@ -1,3 +0,0 @@
-dofile('/etc/nginx/lua/service_provider_init.lua')
-dofile('/etc/nginx/lua/test_init.lua')
-dofile('/etc/nginx/lua/mock_idp_init.lua')
--- nginx-lua-saml-service-provider/tests/cookie_without_domain/etc/nginx/lua/saml/mock_idp/config.lua
+++
@@ -1,9 +0,0 @@
-return {
-    response = {
-        id_attr = {
-            attrName = "ID", nodeName = "Response",
-            nsHref = "urn:oasis:names:tc:SAML:2.0:protocol"
-        },
-        attribute_names = {"mail"},
-    }
-}
